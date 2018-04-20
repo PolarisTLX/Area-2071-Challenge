bc821cfd83aff57efe0de840a54d3c14d2325f07
PolarisTLX <paul.a.rail@gmail.com>
2018-04-20 17:00:01 -0400

    initial javascript animations
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let nebula = document.getElementsByClassName('girl-nebula-img');
+let nebula = document.getElementsByClassName('girl-nebula-img')[0];
+
+
+window.addEventListener('scroll', event => {
+  if (window.pageYOffset > 500) {
+    nebula.style.margin = "auto";
+  }
+  // if (window.pageYOffset > 800) {
+  //   nebula.style.margin = "auto";
+  // }
+});

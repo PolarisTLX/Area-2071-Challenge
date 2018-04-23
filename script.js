813f1681e3a5e3b1980848be9de5af1811c3f841
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-04-23 15:22:10 -0400

    lounge image animation added
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let greyNebula = document.getElementsByClassName('grey-nebula')[0];
 greyNebula.style.width = nebula.width + "px";
 greyNebula.style.height = nebula.height + "px";
 
+let lounge = document.getElementsByClassName('lounge-img')[0];
+let greyLounge = document.getElementsByClassName('grey-lounge')[0];
+greyLounge.style.width = lounge.width + "px";
+greyLounge.style.height = lounge.height + "px";
 
 
 window.addEventListener('scroll', event => {
@@ -10,6 +14,12 @@ window.addEventListener('scroll', event => {
     // nebula.style.margin = "auto";
     nebula.className = "girl-nebula-img move-nebula";
     greyNebula.className = "grey-nebula move-grey-nebula";
+  }
+
+  if (window.pageYOffset > 1200) {
+    // nebula.style.margin = "auto";
+    lounge.className = "lounge-img move-lounge";
+    greyLounge.className = "grey-lounge move-grey-lounge";
   }
   // if (window.pageYOffset > 800) {
   //   nebula.style.margin = "auto";

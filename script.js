f368aa48bf340cfc8cc77052f0fe2358df510676
PolarisTLX <paul.a.rail@gmail.com>
2018-04-23 15:03:11 -0400

    got image animation working
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let nebula = document.getElementsByClassName('girl-nebula-img')[0];
+let greyNebula = document.getElementsByClassName('grey-nebula')[0];
+greyNebula.style.width = nebula.width + "px";
+greyNebula.style.height = nebula.height + "px";
+
 
 
 window.addEventListener('scroll', event => {
   if (window.pageYOffset > 500) {
-    nebula.style.margin = "auto";
+    // nebula.style.margin = "auto";
+    nebula.className = "girl-nebula-img move-nebula";
+    greyNebula.className = "grey-nebula move-grey-nebula";
   }
   // if (window.pageYOffset > 800) {
   //   nebula.style.margin = "auto";
